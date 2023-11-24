0bef688e3fb2eaba6729c2d79e7b71d65959d8ef
Varunsai16 <141404439+Varunsai16@users.noreply.github.com>
2023-11-24 10:37:45 +0530

    Add files via upload

---
+++ Courses.py
@@ -0,0 +1,63 @@
+ds_course = ['Data Analyst',
+             'Data Engineers',
+             'Database Administrator',
+             'Machine Learning Engineer',
+             'Data Scientist',
+             'Data Architect',
+             'Statistician',
+             'Business Analyst',
+             'Data and Analytics Manager',
+             ]
+
+web_course = ['Front-End Developer',
+              'Back-End Developer',
+              'Full-Stack Developer'
+              'UI/UX Designer'
+              'Web Designer',
+              'DevOps Engineer',
+              'Quality Assurance (QA) Engineer',
+              'Web Content Manager',
+              'Web Content Manager']
+
+android_course = ['Android DevOps Engineer',
+                  'Android Architect',
+                  'Android Product Manager',
+                  'Android Security Specialist',
+                  'Android Community Manager',
+                  'Full-Stack Mobile Developer',
+                  'QA Tester for Mobile Apps',
+                  'Mobile UI/UX Designer',
+                  'Cross-Platform Mobile Developer',
+                  'Android Game Developer']
+
+ios_course = ['iOS Developer',
+              'Mobile App UI/UX Designer (iOS)',
+              'iOS Software Engineer',
+              'iOS Architect'
+              'iOS QA Tester'
+              'iOS DevOps Engineer'
+              'iOS Game Developer'
+              'iOS Security Specialist'
+              'iOS Product Manager'
+              'iOS Support Engineer']
+uiux_course = ['UX Writer',
+               'UI/UX Lead or Director',
+               'Product Designer',
+               'UI/UX Designer (Generalist)',
+               'UX Designer',
+               'Product Designer',
+               'Usability Tester',
+               'Information Architect',
+               'ser Researcher',
+               'Design Strategist']
+               
+
+resume_videos = ['https://youtu.be/y8YH0Qbu5h4','https://youtu.be/J-4Fv8nq1iA',
+                 'https://youtu.be/yp693O87GmM','https://youtu.be/UeMmCex9uTU',
+                 'https://youtu.be/dQ7Q8ZdnuN0','https://youtu.be/HQqqQx5BCFY',
+                 'https://youtu.be/CLUsplI4xMU','https://youtu.be/pbczsLkv7Cc']
+
+interview_videos = ['https://youtu.be/Ji46s5BHdr0','https://youtu.be/seVxXHi2YMs',
+                    'https://youtu.be/9FgfsLa_SmY','https://youtu.be/2HQmjLu-6RQ',
+                    'https://youtu.be/DQd_AlIvHUw','https://youtu.be/oVVdezJ0e7w'
+                    'https://youtu.be/JZK1MZwUyUU','https://youtu.be/CyXLhHQS3KY']
